d65a706f23b12a435afafa576d3ba58ad8a98f6a
Gabriel Grubba <gabriel@discourse.org>
2025-02-13 07:54:59 -0300

    dev: update forms dependency

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
-
+import React from "react";
+import { Hello } from "./Hello.jsx";
+import { Info } from "./Info.jsx";
 export const App = () => (
   <div className="max-w-3xl min-h-screen mx-auto sm:pt-10">
-    <Hello/>
-    <Info/>
+    <Hello />
+    <Info />
   </div>
 );
